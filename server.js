14e9eabc174468dc0d9026db1d00c907e16fcbca
kao.ring <kao.ring@yahoo.com>
2020-08-24 20:25:29 -0400

    useUnifiedTopology: true,

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -15,9 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: true,
+// });
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
